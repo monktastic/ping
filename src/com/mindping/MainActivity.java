ff088a3aef52e20cece77dc2321a2e1194bddc29
Aditya Prasad <akprasad@gmail.com>
2012-07-08 13:46:34 -0700

    Saves state for ping button.

--- src/com/mindping/MainActivity.java
+++ src/com/mindping/MainActivity.java
@@ -7,14 +7,19 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
+
+	private PendingIntent sender;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,24 +28,67 @@ public class MainActivity extends Activity {
 
 		final AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 		final Intent intent = new Intent(this, PingReceiver.class);
-		final PendingIntent sender = PendingIntent.getBroadcast(this, 0,
-				intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		sender = PendingIntent.getBroadcast(this, 0, intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		final SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(MainActivity.this);
 
 		final ToggleButton pingButton = (ToggleButton) findViewById(R.id.ping_toggleButton);
+
 		pingButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				if (!pingButton.isChecked()) {
 					// disable future events
 					am.cancel(sender);
 				} else {
+					int minutes = preferences.getInt("ping_interval", 0);
 					Calendar cal = Calendar.getInstance();
-					cal.add(Calendar.SECOND, 3);
+					// cal.add(Calendar.SECOND, minutes);
 					am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
 							sender);
 				}
 			}
 		});
 
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+		final ToggleButton pingButton = (ToggleButton) findViewById(R.id.ping_toggleButton);
+		savedInstanceState.putBoolean("is_pinging", pingButton.isChecked());
+		Toast.makeText(this, "Saving " + pingButton.isChecked(),
+				Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		boolean checked = savedInstanceState.getBoolean("is_pinging");
+		Toast.makeText(this, "2) Value is " + checked, Toast.LENGTH_SHORT)
+				.show();
+		final ToggleButton pingButton = (ToggleButton) findViewById(R.id.ping_toggleButton);
+		pingButton.setChecked(checked);
+	}
+
+	@Override
+	public void onDestroy() {
+		stop();
+		super.onDestroy();
+	}
+
+	@Override
+	public void finish() {
+		stop();
+		super.finish();
+	}
+
+	private void stop() {
+		Toast.makeText(this, "All pings stopped!", Toast.LENGTH_SHORT).show();
+		final AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		if (sender != null) {
+			am.cancel(sender);
+		}
 	}
 
 	@Override
@@ -54,7 +102,6 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(this, SettingsActivity.class);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 		startActivity(intent);
-
 		return true;
 	}
 }
--- src/com/mindping/PingReceiver.java
+++ src/com/mindping/PingReceiver.java
@@ -7,15 +7,17 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 public class PingReceiver extends BroadcastReceiver {
-	final static long[] pulse = { 0, 200, //
-			200, 200, //
-			100, 100, //
-			100, 200, //
-			200, 200, //
+	final static long[] pulse = { 0, 150, //
+			250, 150, //
+			150, 100, //
+			100, 150, //
+			250, 200, //
 	};
 
 	@Override
@@ -28,12 +30,16 @@ public class PingReceiver extends BroadcastReceiver {
 
 			vibrator.vibrate(pulse, -1);
 
+			SharedPreferences preferences = PreferenceManager
+					.getDefaultSharedPreferences(context);
+			int minutes = preferences.getInt("ping_interval", 0);
+
 			PendingIntent sender = PendingIntent.getBroadcast(context, 0,
 					intent, PendingIntent.FLAG_UPDATE_CURRENT);
 			AlarmManager am = (AlarmManager) context
 					.getSystemService(Context.ALARM_SERVICE);
 			Calendar cal = Calendar.getInstance();
-			cal.add(Calendar.SECOND, 3);
+			cal.add(Calendar.SECOND, minutes);
 			am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), sender);
 		} catch (Exception e) {
 			Toast.makeText(context, "ERRAR!: " + e, Toast.LENGTH_SHORT).show();
