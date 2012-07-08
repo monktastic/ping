a63bc2fd76bbe7790202ffc0cd3b541237bc9458
Aditya Prasad <akprasad@gmail.com>
2012-07-08 14:17:09 -0700

    Moving ping to correct location. Making it sound on pings.

--- src/com/mindping/PingReceiver.java
+++ src/com/mindping/PingReceiver.java
@@ -8,27 +8,40 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 public class PingReceiver extends BroadcastReceiver {
-	final static long[] pulse = { 0, 150, //
+	final static long[] SHAVE_AND_HAIRCUT_PULSE = { 0, 150, //
 			250, 150, //
 			150, 100, //
 			100, 150, //
 			250, 200, //
 	};
 
+	final static long[] PING_PULSE = { 0, //
+			50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, };
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		try {
+			AssetFileDescriptor afd = context.getAssets().openFd(
+					"simple_ping.wav");
+			MediaPlayer player = new MediaPlayer();
+			player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(),
+					afd.getLength());
+			player.prepare();
+			player.start();
+
 			Toast.makeText(context, "Are you aware?", Toast.LENGTH_SHORT)
 					.show();
 			Vibrator vibrator = (Vibrator) context
 					.getSystemService(Context.VIBRATOR_SERVICE);
 
-			vibrator.vibrate(pulse, -1);
+			vibrator.vibrate(PING_PULSE, -1);
 
 			SharedPreferences preferences = PreferenceManager
 					.getDefaultSharedPreferences(context);
